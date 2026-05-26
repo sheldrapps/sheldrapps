1968f955477cbc8b5b830b074f71c6a0bba61249
sheldrapps <sheldrapps@gmail.com>
2026-05-26 01:20:03 -0600

    feat: fix ui for policies

--- apps/sheldrapps-web/src/app/pages/privacy-ecc/privacy-ecc.page.ts
+++ apps/sheldrapps-web/src/app/pages/privacy-ecc/privacy-ecc.page.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { SiteTitleControlsComponent } from '../../components/site-title-controls/site-title-controls.component';
 
 @Component({
   selector: 'app-privacy-ecc-page',
   standalone: true,
-  imports: [TranslateModule, RouterLink],
+  imports: [TranslateModule, RouterLink, SiteTitleControlsComponent],
   templateUrl: './privacy-ecc.page.html',
   styleUrl: './privacy-ecc.page.scss'
 })
--- apps/sheldrapps-web/src/app/pages/privacy-pcm/privacy-pcm.page.ts
+++ apps/sheldrapps-web/src/app/pages/privacy-pcm/privacy-pcm.page.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { SiteTitleControlsComponent } from '../../components/site-title-controls/site-title-controls.component';
 
 @Component({
   selector: 'app-privacy-pcm-page',
   standalone: true,
-  imports: [TranslateModule, RouterLink],
+  imports: [TranslateModule, RouterLink, SiteTitleControlsComponent],
   templateUrl: './privacy-pcm.page.html',
   styleUrl: './privacy-pcm.page.scss'
 })
